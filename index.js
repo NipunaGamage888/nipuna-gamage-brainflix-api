864278825784074c88370e83f5b6e7897dbca381
NipunaGamage888 <nipunagamage888@gmail.com>
2024-03-24 18:45:08 -0400

    Completed my api calls by adding all the info to my data folder

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express= require('express');
-const app =express();
-const cors= require('cors');
-const routes = require('./routes/videos');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const routes = require("./routes/videos");
 
 app.use(cors());
-app.use(express.json())
-const PORT=8080
+app.use(express.json());
+const PORT = 8080;
+app.use("/image", express.static("./public"));
 
-app.use('/api', routes);
+app.use("/api", routes);
 
-app.listen(PORT,()=>console.log('the server is running'))
+app.listen(PORT, () => console.log("the server is running"));
--- routes/videos.js
+++ routes/videos.js
@@ -6,57 +6,57 @@ const fs = require("fs");
 const app = express();
 const router = express.Router();
 
-const api_key = "?api_key=777219bc-bf2e-4b39-9a97-8cefeb6d3047";
-
 app.use(express.static("./public"));
+function readVideos() {
+  const videoData = fs.readFileSync("./data/videos.json");
+  const parsedData = JSON.parse(videoData);
+  console.log(parsedData)
+  return parsedData;
+}
 
-router.get("/videos", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://unit-3-project-api-0a5620414506.herokuapp.com/videos${api_key}`
-    );
-    const data = response.data;
-    res.json(data);
+
+router.get('/videos', (req, res) => {
+  try {     
+    const videos = readVideos();
+    res.json(videos);
   } catch (error) {
-    console.error("Error fetching data:", error);
+    console.error("Error retrieving videos:", error);
   }
 });
 
 router.get("/videos/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const response = await axios.get(
-      `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${id}${api_key}`
-    );
-    res.json(response.data);
+    const response = readVideos()
+    const video= response.find(video=>video.id===id)
+    res.json(video)
   } catch (error) {
     console.error("Error adding comment:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-function readVideos() {
-    const videoData = fs.readFileSync('./data/videos.json');
-    const parsedData = JSON.parse(videoData);
-    return parsedData;
-  }
 
 router.post("/videos", async (req, res) => {
   try {
-    
-    const videoImage='/images/Upload-video-preview.jpg'
+    const videoImage = `/image/Upload-video-preview.jpg`;
     const { title, description } = req.body;
     const newVideo = {
       id: uuidv4(),
       title,
+      channel:"RyanHernandez",
       image: videoImage,
       description,
+      views:0,
+      likes:0,
+      duration:0,
+      comments:[],
       timestamp: Date.now(),
     };
 
-    const video=readVideos()
-    video.push(newVideo)
-    fs.writeFileSync('./data/videos.json', JSON.stringify(video))
-   
+    const video = readVideos();
+    video.push(newVideo);
+    fs.writeFileSync("./data/videos.json", JSON.stringify(video));
+
     res.json(newVideo);
   } catch (error) {
     console.error("Error adding new video:", error);
